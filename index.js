4e37bf9c4c13248b093fa503abbbcd246837928b
4512yasir <yasiryaska1@gmail.com>
2025-03-31 16:52:33 +0300

    adding-behavior-OOP

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return this.sex === "male" 
+        ? `It's me! ${this.name}, the parrot!` 
+        : `${this.name} says squawk!`;
+    }
+  }
+  
+  // Example Usage
+  const felix = new Cat("Felix", "male");
+  console.log(felix.speak()); // Felix says meow!
+  
+  const rex = new Dog("Rex", "male");
+  console.log(rex.speak()); // Rex says woof!
+  
+  const polly = new Bird("Polly", "female");
+  console.log(polly.speak()); // Polly says squawk!
+  
+  const coco = new Bird("Coco", "male");
+  console.log(coco.speak()); // It's me! Coco, the parrot!
+  
